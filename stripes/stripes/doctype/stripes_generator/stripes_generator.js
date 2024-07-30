fa0e310b7d51efc5c2624476f637bd9bb0de25bf
Saif Ur Rehman <saif@paralogic.io>
2024-07-31 02:04:04 +0500

    feat: Stripes Generator DocType

---
+++ stripes/stripes/doctype/stripes_generator/stripes_generator.js
@@ -0,0 +1,60 @@
+// Copyright (c) 2024, ParaLogic and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Stripes Generator', {
+	setup(frm) {
+		frm.page.toggle_sidebar(false);
+	},
+
+	refresh(frm) {
+		frm.disable_save();
+		frm.events.get_stripes_svg(frm);
+	},
+
+	from_date(frm) {
+		frm.events.get_stripes_svg(frm);
+	},
+
+	to_date(frm) {
+		frm.events.get_stripes_svg(frm);
+	},
+
+	air_monitor(frm) {
+		frm.events.get_stripes_svg(frm);
+	},
+
+	get_stripes_svg(frm) {
+		if (!frm.doc.from_date || !frm.doc.to_date) {
+			frm.events.set_message(frm, "Please set filters");
+			return;
+		}
+
+		frm.events.set_message(frm, "Loading...");
+		return frappe.call({
+			method: "stripes.svg.get_stripes_svg",
+			args: {
+				from_date: frm.doc.from_date,
+				to_date: frm.doc.to_date,
+				air_monitor: frm.doc.air_monitor,
+			},
+			callback(r) {
+				let wrapper = $(frm.fields_dict.svg_display.wrapper);
+				wrapper.empty();
+
+				if (r.message) {
+					$(r.message).appendTo(wrapper);
+				}
+			},
+			error() {
+				frm.events.set_message(frm, "An error occurred");
+			},
+		})
+	},
+
+	set_message(frm, message) {
+		let wrapper = $(frm.fields_dict.svg_display.wrapper);
+
+		wrapper.empty();
+		$(`<div style="margin-bottom: 5px; color: var(--text-muted);">${message}</div>`).appendTo(wrapper);
+	}
+});
