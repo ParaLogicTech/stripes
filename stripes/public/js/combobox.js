adbc34bbb98eb2e8fd443740099cc6f2bd415801
Wasiq-Ali <wasiqali878@gmail.com>
2024-09-09 15:43:43 +0500

    fix: combobox item selection & minor change.

--- stripes/public/js/combobox.js
+++ stripes/public/js/combobox.js
@@ -1,61 +1,69 @@
 class Combobox {
-	constructor(wrapper) {
+	constructor(wrapper, on_change) {
 		this.wrapper = document.querySelector(wrapper);
-		this.dropdown = document.querySelector(".dropdown");
-		this.dropdown_menu = document.querySelector(".dropdown-menu");
-		this.dropdown_buttons = document.querySelectorAll(".dropdown-toggle");
-		this.input = this.wrapper.querySelector("input");
+
+		this.dropdown = this.wrapper.closest(".dropdown");
+		this.dropdown_menu = this.dropdown.querySelector(".dropdown-menu");
+		this.dropdown_button = this.dropdown.querySelector(".dropdown-toggle");
+
+		this.search = this.wrapper.querySelector("input");
 		this.list_items_container = this.wrapper.querySelector("ul.list");
 		this.list_items = Array.from(this.wrapper.querySelectorAll("ul.list li"));
 		this.current_focus = -1;
+
+		this.on_change = on_change;
+		this.current_value = null;
 
 		this.initialize();
 	}
 
 	initialize() {
-		this.items = this.list_items.map(item => ({ name: item.textContent.trim(), element: item }));
+		this.items = this.list_items.map(item => ({ name: item.textContent.trim(), element: item } ));
 
 		this.fuse = new Fuse(this.items, {
 			keys: ["name"],
-			threshold: 0.0,
+			threshold: 0.3,
 			includeMatches: true,
 			minMatchCharLength: 1,
+			location: 0,
+			distance: 10,
 		});
 
 		this.bind_events();
-		this.show_items(this.list_items);
-		this.toggle_item_selection();
+		this.render_items(this.list_items);
 	}
 
 	bind_events() {
-		this.input.addEventListener("input", () => this.filter_items());
-		this.input.addEventListener('keydown', (e) => this.handle_keyboard_controls(e));
-		this.wrapper.addEventListener('click', (e) => this.input.focus());
-		this.dropdown_menu.addEventListener('click', (e) => {
-			this.input.focus();
-			this.remove_highlight(this.list_items);
-		});
+		this.search.addEventListener("input", () => this.filter_items());
+		this.search.addEventListener('keydown', (e) => this.handle_keyboard_controls(e));
 
-		// Input Search focus
-		this.dropdown_buttons.forEach(dropdown => {
-			dropdown.addEventListener("click", () => this.input.focus());
+		// Input Focus
+		this.dropdown_menu.addEventListener("click", () => setTimeout(() => {this.search.focus()}, 0));
+		this.dropdown_button.addEventListener("click", () => setTimeout(() => {this.search.focus()}, 0));
+
+		// Bind Item Selection
+		this.list_items.forEach(item => {
+			item.addEventListener("click", (e) => {
+				this.set_value(item.innerText);
+				this.on_change?.(this.current_value);
+			});
 		});
 	}
 
 	filter_items() {
-		const query = this.input.value.trim();
+		const query = this.search.value.trim();
 		const results = query ? this.fuse.search(query).map(result => result.item.element) : this.list_items;
-		this.show_items(results);
+		this.render_items(results);
 	}
 
-	show_items(items) {
-		this.check_items_existence(items);
+	render_items(items) {
+		this.check_item_existence(items);
 
 		this.list_items.forEach(item => item.style.display = "none");
 		items.forEach(item => item.style.display = "flex");
 	}
 
-	check_items_existence(items) {
+	check_item_existence(items) {
 		this.clear_message();
 
 		if (!items.length) {
@@ -91,7 +99,9 @@ class Combobox {
 		}
 
 		if (e.key === "Escape") {
-			this.input.blur();
+			this.search.blur();
+
+			// Hide Drop Down
 			this.dropdown.classList.remove("show");
 			this.dropdown_menu.classList.remove("show");
 		}
@@ -124,31 +134,33 @@ class Combobox {
 		this.list_items_container.appendChild(h4);
 	}
 
-	toggle_item_selection() {
-		this.list_items.forEach(item => {
-			item.addEventListener("click", () => {
-				const is_selected = item.classList.contains("selected");
-	
+	set_value(value) {
+		this.current_value = value;
+		this.update_selected_in_view();
+	}
+
+	update_selected_in_view() {
+		for (let item of this.list_items) {
+			const item_value = item.innerText;
+			const is_selected = item.classList.contains("selected");
+
+			if (this.current_value == item_value) {
 				if(is_selected) {
 					item.classList.remove("selected");
 					item.querySelector("img")?.remove();
+					this.dropdown_button.querySelector("span").innerText = "Select"
 				} else {
-					this.reset_item_selection();
-
-					// Select the clicked item
 					if (!item.querySelector("img")) {
 						item.insertAdjacentHTML("beforeend", `<img src="/assets/stripes/images/icons/check.svg" alt="check">`);
 					}
 					item.classList.add("selected");
+					// Change the button text after select a item.
+					this.dropdown_button.querySelector("span").innerText = this.current_value;
 				}
-			});
-		});
-	}
-
-	reset_item_selection() {
-		this.list_items.forEach(i => {
-			i.classList.remove("selected");
-			i.querySelector("img")?.remove();
-		});
+			} else {
+				item.classList.remove("selected");
+				item.querySelector("img")?.remove();
+			}
+		}
 	}
 }
