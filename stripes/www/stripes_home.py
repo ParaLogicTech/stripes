95e635e768e99eee67704f016a64326c466ad7c9
Wasiq-Ali <wasiqali878@gmail.com>
2024-10-10 15:12:18 +0500

    fix: permalink issues and improved popover font size.

--- stripes/www/stripes_home.py
+++ stripes/www/stripes_home.py
@@ -2,4 +2,10 @@ import frappe
 from stripes.svg import get_stripes_svg
 
 def get_context(context):
-	context.stripes = get_stripes_svg("2018-01-01", "2018-12-31")
+	region = frappe.local.request.path.split("/")[-1]
+	if region:
+		context.stripes = get_stripes_svg("2018-01-01", "2018-12-31", region)
+		context.selected_region = region
+	else:
+		context.stripes = get_stripes_svg("2018-01-01", "2018-12-31")
+		context.selected_region = "Global"
