ec9e3b4e2db5925dcc17af9d9093913d66cb0e69
Wasiq-Ali <wasiqali878@gmail.com>
2024-10-21 17:30:03 +0500

    fix: improvements.

--- stripes/www/stripes_home.py
+++ stripes/www/stripes_home.py
@@ -1,14 +1,27 @@
 import frappe
 from stripes.image import get_stripes_svg
+from urllib.parse import quote, urljoin
 
 def get_context(context):
+	context.title = "Pollution Stripes"
+
 	# Extract the region from the URL path
 	region = frappe.form_dict.get('region')
 
-	# Default context values
-	context.stripes = get_stripes_svg("2018-01-01", "2018-12-31")
-	context.selected_region = "Global"
+	context.selected_region = region or "Global"
 
+	context.from_date = "2018-01-01"
+	context.to_date = "2018-12-31"
+
+	context.image_api = f"/api/method/stripes.image.get_stripes_png_image"
+	context.image_params = f"?from_date={context.from_date}&to_date={context.to_date}"
 	if region:
-		context.stripes = get_stripes_svg("2018-01-01", "2018-12-31", region)
-		context.selected_region = region
+		context.image_params += f"&monitor_region={quote(region)}"
+
+	context.image_url = f"{context.image_api}{context.image_params}"
+	context.image = frappe.utils.get_url(context.image_api) + context.image_params
+
+	try:
+		context.stripes_svg = get_stripes_svg(context.from_date, context.to_date, monitor_region=region)
+	except frappe.DoesNotExistError:
+		raise frappe.PageDoesNotExistError
